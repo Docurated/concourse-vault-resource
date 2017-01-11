a0f63c5698d563cd56e1762e896b8e59651ea157
Seraf <nephilim.seraphin@gmail.com>
2017-01-11 13:52:40 +0100

    approle: Add AppRole authentication method
    This commit add the ability to authenticate on the approle endpoint.
    On a shared concourse ci, you may not want to use a common certificate which will give the same rights to every pipelines/teams.
    
    Using a specific policy for a role allow to have more granular ACL across your pipelines.
    
    To not break default behavior and existing pipelines, I kept the aws-ec2 authentication as default.

--- assets/common.sh
+++ assets/common.sh
@@ -1,8 +1,18 @@
-login() {
+login_aws_ec2() {
     vault_role="$1"
     vault_nonce="$2"
     cert=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/pkcs7 | tr -d '\n')
     token=$(vault write -format=json auth/aws-ec2/login role=${vault_role} pkcs7=${cert} nonce=${vault_nonce} | jq -r '.auth.client_token')
+    if [ -z "${token}" ]; then
+        echo "ERROR: No token retrieved"
+    fi
+    echo -n "${token}" > ~/.vault-token
+}
+
+login_approle() {
+    vault_role_id="$1"
+    vault_secret_id="$2"
+    token=$(vault write -format=json auth/approle/login role_id=${vault_role_id} secret_id=${vault_secret_id} | jq -r '.auth.client_token')
     if [ -z "${token}" ]; then
         echo "ERROR: No token retrieved"
     fi
